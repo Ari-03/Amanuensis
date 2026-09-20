36a94a4b623b2d99a4053e87d099f2940399c788
Aritra Das <d.aritra@outlook.com>
2026-09-19 21:28:54 -0700

    Initial Commit

---
+++ Amanuensis/AmanuensisApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmanuensisApp.swift
+//  Amanuensis
+//
+//  Created by Aritra Das on 9/19/26.
+//
+
+import SwiftUI
+
+@main
+struct AmanuensisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
